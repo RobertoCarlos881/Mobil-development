f2c5aa3d75261b0e820862d5e96a832d3cca6677
RobertoCarlos881 <roberto881carlos881@hotmail.com>
2022-06-21 16:46:50 -0500

    Proyecto 3

---
+++ Proyecto3/AR/AR/ARApp/app/src/main/java/com/wheic/arapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wheic.arapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
